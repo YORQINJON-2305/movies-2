70d33fb8dc0642aa0ed0b336324446050457da64
YORQINJON-2305 <jakbaraliyevyorqinjon505@gmail.com>
2022-11-01 10:16:11 +0500

    Add search year

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const categories = [];
 
+
 //Select
 const elSelectCategory = document.querySelector(".select-js");
 const templateOption = document.querySelector(".option-temp").content;
@@ -11,6 +12,10 @@ const templateFragment = document.querySelector(".movie-temp").content;
 //Search form
 const elSearchForm = document.querySelector(".search-form");
 const elSearchInput = elSearchForm.querySelector(".search-input");
+
+const elSearchYearForm = document.querySelector(".search-year-form");
+const elStartYearInput = elSearchYearForm.querySelector(".start-year");
+const elEndYearInput = elSearchYearForm.querySelector(".end-year");
 
 //Modal
 const elModal = document.querySelector(".modal-js");
@@ -25,7 +30,7 @@ const modalLink = elModal.querySelector(".modal-imdb-link");
 
 const fragment = new DocumentFragment();
 
-const sliceMovies = movies.slice(0, 20);
+const sliceMovies = fullMovies.slice(0, 20);
 
 //Minutni soatga aylantirish
 function getDuration(time){
@@ -46,12 +51,12 @@ function viewMovie(movies){
     movies.forEach(movie => {
         templateFragmentClone = templateFragment.cloneNode(true);
 
-        templateFragmentClone.querySelector(".movie-img").src = `http://i3.ytimg.com/vi/${movie.ytid}/mqdefault.jpg`;
-        templateFragmentClone.querySelector(".movie-title").textContent = movie.Title;
-        templateFragmentClone.querySelector(".movie-year").textContent = movie.movie_year;
+        templateFragmentClone.querySelector(".movie-img").src = movie.yt_img_md;
+        templateFragmentClone.querySelector(".movie-title").textContent = movie.title;
+        templateFragmentClone.querySelector(".movie-year").textContent = movie.year;
         templateFragmentClone.querySelector(".movie-rating").textContent = movie.imdb_rating;
         templateFragmentClone.querySelector(".movie-duration").textContent = getDuration(movie.runtime);
-        templateFragmentClone.querySelector(".category").textContent = getSplitCategory(movie.Categories);
+        templateFragmentClone.querySelector(".category").textContent = getSplitCategory(movie.categories);
         templateFragmentClone.querySelector(".info-btn").dataset.ytid = movie.ytid;
 
         fragment.appendChild(templateFragmentClone);
@@ -60,6 +65,23 @@ function viewMovie(movies){
     elList.appendChild(fragment);
 }
 
+elSearchYearForm.addEventListener("submit", searchYear)
+
+function searchYear(evt){
+    evt.preventDefault();
+    const startInputValue = Number(elStartYearInput.value.trim())
+    const endInputValue = Number(elEndYearInput.value.trim());
+    console.log(startInputValue, endInputValue  )
+    const searchYearMovie = fullMovies.filter(item => ((startInputValue <= item.year && endInputValue >= item.year) || (startInputValue == "" && endInputValue >= item.year) || (startInputValue <= item.year && endInputValue == "")));
+    if (searchYearMovie.length > 0){
+        viewMovie(searchYearMovie);
+    }  else {
+        elList.innerHTML = "Movie not found !!!"
+
+    }
+}
+
+
 //Qidirilgan kinoni topish
 elSearchForm.addEventListener("submit", searchFunc);
 
@@ -67,14 +89,22 @@ elSearchForm.addEventListener("submit", searchFunc);
 function searchFunc(evt){
     evt.preventDefault();
     const searchInputValue = elSearchInput.value.trim();
+
     const regexTitle = new RegExp(searchInputValue, "gi");
-    const searchMovie = movies.filter(item => String(item.Title).match(regexTitle));
+    const searchMovie = fullMovies.filter(item => item.title.match(regexTitle));
         if(searchMovie.length > 0){
             viewMovie(searchMovie);
         }  else{
             elList.innerHTML = "Movie not found !!!"
         }
+    // const searchYear = fullMovies.filter(item => );
+    // if(searchYear.length > 0){
+    //     viewMovie(searchYear);
+    // }  else{
+    //     elList.innerHTML = "Movie not found !!!"
+    // }
 }
+
 
 //Category bo'yicha saralash
 elSelectCategory.addEventListener("click", searchCategories)
@@ -82,17 +112,17 @@ elSelectCategory.addEventListener("click", searchCategories)
 //Category bo'yicha saralash funksiyasi
 function searchCategories(){
     const selectValue = elSelectCategory.value
-    const searchCategory = movies.filter(item => item.Categories.match(selectValue));
+    const searchCategory = fullMovies.filter(item => item.categories.match(selectValue));
     if (searchCategory.length > 0){
         viewMovie(searchCategory);
     }   else{
-        viewMovie(sliceMovies)
+        viewMovie(sliceMovies);
     }
 }
 
 //Categorylarni arrayga push qilib olish
-movies.forEach(allObj => {
-    allObj.Categories.split("|").forEach(allCategories => {
+fullMovies.forEach(allObj => {
+    allObj.categories.split("|").forEach(allCategories => {
         if (!categories.includes(allCategories)){
             categories.push(allCategories);
         }
@@ -116,21 +146,21 @@ elList.addEventListener("click", function(evt){
     const getTargetEl = evt.target;
     if(getTargetEl.matches(".info-btn")){
         const btnId = getTargetEl.dataset.ytid;
-        const foundMovie = movies.find(movie => movie.ytid == btnId);
+        const foundMovie = fullMovies.find(movie => movie.ytid == btnId);
         modalInfo(foundMovie);
-        }
+    }
 })
 
 //Topilgan objectni modalga chizish
 function modalInfo(foundMovie){
-    modalTitle.textContent = foundMovie.Title;
-    modalIframe.src = `https://www.youtube-nocookie.com/embed/${foundMovie.ytid}`;
+    modalTitle.textContent = foundMovie.title;
+    modalIframe.src = foundMovie.yt_iframe;
     modalRating.textContent = foundMovie.imdb_rating;
-    modalYear.textContent = foundMovie.movie_year;
+    modalYear.textContent = foundMovie.year;
     modalRuntime.textContent = getDuration(foundMovie.runtime);
-    modalCategories.textContent = foundMovie.Categories.split("|").join(", ");
+    modalCategories.textContent = foundMovie.categories.split("|").join(", ");
     modalSummary.textContent = foundMovie.summary;
-    modalLink.href = `https://www.imdb.com/title/${foundMovie.imdb_id}`;
+    modalLink.href = foundMovie.imdb_link;
 }
 
 //Modalda qolib iframeni srcni tozalab yuborish
